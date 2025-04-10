142bd25286bd897020bfbdda21aea5720e54ff9f
AliKosheev <kosheevali70@gmail.com>
2025-04-10 23:12:45 +0300

    аываываываф

--- src/components/ui/AnimatedBlob.tsx
+++ src/components/ui/AnimatedBlob.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import anime from "animejs"; // ✅ ничего не трогаем, просто стандартный импорт
+import * as anime from "animejs"; // 👈 универсально для TypeScript + Vite
 
 const paths = [
   "M417,314Q413,378,358.5,417Q304,456,248,444Q192,432,151,396.5Q110,361,76,311.5Q42,262,59.5,195.5Q77,129,133,98.5Q189,68,251,60Q313,52,361,94.5Q409,137,417,195.5Q425,254,417,314Z",
@@ -12,7 +12,7 @@ export const AnimatedBlob = () => {
   useEffect(() => {
     if (!pathRef.current) return;
 
-    anime({
+    anime.default({
       targets: pathRef.current,
       d: [
         { value: paths[1] },
