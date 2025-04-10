fa03e0c92ea5aa1deb6858a8115740c093038d90
AliKosheev <kosheevali70@gmail.com>
2025-04-10 12:40:33 +0300

    hots

--- src/components/ui/dots-grid.tsx
+++ src/components/ui/dots-grid.tsx
@@ -11,7 +11,7 @@ export function DotsGrid(props: React.ComponentProps<"svg">) {
         className={props.className}
       >
         <defs>
-          <pattern id="dots" x="0" y="0" width="10" height="10" patternUnits="userSpaceOnUse">
+          <pattern id="dots" x="0" y="0" width="3" height="3" patternUnits="userSpaceOnUse">
             <circle cx="1" cy="1" r="1" fill="#ffffff" opacity="0.07" />
           </pattern>
         </defs>
