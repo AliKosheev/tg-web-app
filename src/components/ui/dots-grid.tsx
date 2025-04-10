7ead030d1b83a2978ad63016e4015a9237c1b959
AliKosheev <kosheevali70@gmail.com>
2025-04-10 23:43:36 +0300

    78676876786

--- src/components/ui/dots-grid.tsx
+++ src/components/ui/dots-grid.tsx
@@ -7,7 +7,7 @@ export default function DotsGrid({ className }: { className?: string }) {
       className={clsx("pointer-events-none absolute inset-0 z-0", className)}
       style={{
         backgroundImage:
-          "radial-gradient(#3a3a3a 1.13px, transparent 1.13px)",
+          "radial-gradient(#3a3a3a 1.5px, transparent 1.5px)",
         backgroundSize: "9px 9px",
       }}
     />
