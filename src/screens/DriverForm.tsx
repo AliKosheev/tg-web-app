cdb9e17783d9b6313e328efda6f884596f36d1ad
AliKosheev <kosheevali70@gmail.com>
2025-04-18 18:54:38 +0300

    gfhjk

--- src/screens/DriverForm.tsx
+++ src/screens/DriverForm.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
-import TopBar from "@/components/TopBar";
+import TopBar from "@/components/ui/TopBar";
 import DotsGrid from "@/components/ui/dots-grid";
 
 export default function DriverForm() {
--- src/screens/PassengerScreen.tsx
+++ src/screens/PassengerScreen.tsx
@@ -1,4 +1,4 @@
-import TopBar from "@/components/TopBar";
+import TopBar from "@/components/ui/TopBar";
 import DotsGrid from "@/components/ui/dots-grid";
 
 export default function PassengerScreen() {
