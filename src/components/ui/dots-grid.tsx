eec629f9d9b183c7bb418241eaebbea7fc833ef6
AliKosheev <kosheevali70@gmail.com>
2025-04-18 14:38:36 +0300

    dasdasd

--- src/components/ui/dots-grid.tsx
+++ src/components/ui/dots-grid.tsx
@@ -17,7 +17,7 @@ export default function DotsGrid({ className }: { className?: string }) {
       <div className="absolute bottom-0 left-0 w-full h-48 bg-gradient-to-t from-black via-black/80 to-transparent z-10" />
 
       {/* Нижнее фиолетовое свечение */}
-      <div className="absolute bottom-[-25%] left-1/2 -translate-x-1/2 w-[150%] h-64 bg-gradient-to-t from-violet-800 via-indigo-900 to-transparent blur-3xl opacity-40 z-0" />
+      <div className="absolute bottom-[-35%] left-1/2 -translate-x-1/2 w-[150%] h-64 bg-gradient-to-t from-violet-800 via-indigo-900 to-transparent blur-3xl opacity-40 z-0" />
     </div>
   );
 }
